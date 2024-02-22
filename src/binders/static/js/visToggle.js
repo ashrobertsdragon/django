5a09ccc9898df2a2ca12f84e18129d54ea1af98e
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-02-22 13:54:32 -0700

    moved visibility toggle javascript to standalone script instead of inline to reusse it for signup and login pages

---
+++ src/binders/static/js/visToggle.js
@@ -0,0 +1,15 @@
+function toggleVisibility() {
+  var apiKeyInput = document.getElementById("user_key");
+  var currentType = apiKeyInput.getAttribute("type");
+  apiKeyInput.setAttribute("type", currentType === "password" ? "text" : "password");
+  var visibilityImage = document.getElementById("visibilityImage");
+  if (currentType === "password") {
+// If the password is visible, set the image to invisible.png
+    visibilityImage.src = "static/invisible.png";
+    visibilityImage.alt = "hide key"
+  } else {
+// If the password is hidden, set the image to visibility.png
+    visibilityImage.src = "static/visibility.png";
+    visibilityImage.alt = "show key"
+  }
+}
