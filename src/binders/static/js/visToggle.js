623d72f06a78da82d9ba46ba31a2f28739d5beca
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-02-25 00:54:26 -0700

    updated visibility toggle to Google Material Symbols instead of icons

--- src/binders/static/js/visToggle.js
+++ src/binders/static/js/visToggle.js
@@ -1,15 +1,15 @@
 function toggleVisibility() {
   var apiKeyInput = document.getElementById("user_key");
-  var currentType = apiKeyInput.getAttribute("type");
-  apiKeyInput.setAttribute("type", currentType === "password" ? "text" : "password");
-  var visibilityImage = document.getElementById("visibilityImage");
-  if (currentType === "password") {
-// If the password is visible, set the image to invisible.png
-    visibilityImage.src = "static/images/invisible.png";
-    visibilityImage.alt = "hide key"
-  } else {
-// If the password is hidden, set the image to visibility.png
-    visibilityImage.src = "static/images/visibility.png";
-    visibilityImage.alt = "show key"
+  var passwordInput = document.getElementById("password");
+
+  var inputElement = apiKeyInput ? apiKeyInput : passwordInput;
+  if (inputElement) {
+    var currentType = inputElement.getAttribute("type");
+    inputElement.setAttribute("type", currentType === "password" ? "text" : "password");
+    
+    var visibilitySpan = document.getElementById("eye");
+    if (visibilitySpan) {
+      visibilitySpan.textContent = currentType === "password" ? "visibility_off" : "visibility";
+    }
   }
 }
